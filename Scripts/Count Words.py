0c32d7a3e86176faca2498f4a25584ae3d0da2eb
Gun.io Whitespace Robot <contact@gun.io>
2011-10-21 03:30:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- Scripts/Count Words.py
+++ Scripts/Count Words.py
@@ -3,10 +3,10 @@ from AppKit import *
 
 # app-defined main entry point
 def main(textView):
-	
+
 	# get word count using AppKit's own functionality (via NSTextStorage)
 	word_count = textView.textStorage().words().count()
-	
+
 	# show an alert panel telling the word count in a nice way
 	words_str = 'There are %d words in this document' % word_count
 	NSRunAlertPanel('Words in Document', words_str, None, None, None)
--- Scripts/Use List-format.py
+++ Scripts/Use List-format.py
@@ -3,13 +3,13 @@ from AppKit import *
 
 # app-defined main entry point
 def main(textView):
-	
+
 	# get the text lines as a list
 	lines = textView.textStorage().string().split('\n')
-	
+
 	# prepend a dash to each line
 	lines = ['- ' + line for line in lines]
-	
+
 	# re-set the text contents to the newly arranged list
 	new_string = '\n'.join(lines)
 	textView.textStorage().mutableString().setString_(new_string)
